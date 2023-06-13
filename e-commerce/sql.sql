1f5401e4543badc4fb5263a3a212464a39b930f9
Danielle Farias <103695058+DanielleFMedeiros@users.noreply.github.com>
2023-06-12 22:59:58 -0300

    Update.sql

--- e-commerce/sql.sql
+++ e-commerce/sql.sql
@@ -1,3 +1,5 @@
+
+
 --#8
 select 
 	CIDADE.nome as CIDADE,
@@ -26,8 +28,50 @@ inner join PRODUTO on
 	PRODUTO.ID = VENDA.id;
 --#10
 
-select
-	PRODUTO.nome as NOME_PROD, CATEGORIA.descricao
-from
-	PRODUTO
-inner join categoria  on PRODUTO.ID  = categoria.id;
+SELECT 
+    P.nome, C.DESCRICAO
+FROM
+    categoria c
+        LEFT JOIN
+    produto p ON c.id = p.id;
+
+
+--#11
+SELECT 
+    categoria.descricao AS categoria,
+    SUM(venda_prod.qtde) AS total_vendido
+FROM
+    venda_prod
+        JOIN
+    produto ON produto.id = venda_prod.produto
+        JOIN
+    categoria ON categoria.id = produto.categoria
+GROUP BY categoria.descricao;
+
+--#12
+
+SELECT DATE(data_venda) AS data, COUNT(*) AS total_vendas
+FROM venda
+WHERE data_venda BETWEEN '2023-01-01' AND '2023-12-31'
+GROUP BY DATE(data_venda);
+
+
+--#13
+SELECT 
+    c1.nome, venda.data_venda, produto.nome, telefone.NUMERO
+FROM
+    venda
+        INNER JOIN
+    cliente c1 ON c1.id = venda.cliente
+        INNER JOIN
+    venda_prod ON venda_prod.venda = venda.id
+        INNER JOIN
+    produto ON produto.id = venda_prod.produto
+        INNER JOIN
+    cliente c2 ON c2.id = c1.id
+        INNER JOIN
+    telefone ON telefone.id = c2.id
+WHERE
+    data_venda = '2023-06-12';
+
+
